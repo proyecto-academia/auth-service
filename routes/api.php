6ba6250d9e08085f0b20536486c8bc4ffaf42ec9
Marc Mollá Lillo <marmollil@alu.edu.gva.es>
2025-05-18 19:54:00 +0200

    Remove personal access tokens migration and update deployment configuration with environment setup and cache clearing tasks

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,15 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', [
+    'uses' => function () {
+        return response()->json([
+            'message' => 'Welcome to the API',
+            'status' => 200,
+        ]);
+    },
+]);
+
 
 Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
 Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
